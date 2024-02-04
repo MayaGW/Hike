8900125ff10f5db7b25efbd8908be159eb376a26
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-02-04 12:29:01 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Maya Ghamloush on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
